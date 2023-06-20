f745eef7034062007e17693589ed182c4a5f7267
Niziol <niziol.luk@gmail.com>
2023-06-20 15:50:50 +0200

    changing useState to useReducer for better state management

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,41 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import { reducerActions } from '../../types/Enum';
+
+const emailReducer = (state, action) => {
+	switch (action.type) {
+		case reducerActions.USER_INPUT:
+			return { value: action.value, isValid: action.value.includes('@') };
+		case reducerActions.INPUT_BLUR:
+			return { value: state.value, isValid: state.value.includes('@') };
+		default:
+			return { value: '', isValid: false };
+	}
+};
+
+const passwordReducer = (state, action) => {
+	switch (action.type) {
+		case reducerActions.USER_INPUT:
+			return { value: action.value, isValid: action.value.trim().length > 6 };
+		case reducerActions.INPUT_BLUR:
+			return { value: state.value, isValid: state.value.trim().length > 6 };
+		default:
+			return { value: '', isValid: false };
+	}
+};
 
 const Login = (props) => {
-	const [enteredEmail, setEnteredEmail] = useState('');
-	const [emailIsValid, setEmailIsValid] = useState();
-	const [enteredPassword, setEnteredPassword] = useState('');
-	const [passwordIsValid, setPasswordIsValid] = useState();
 	const [formIsValid, setFormIsValid] = useState(false);
 
+	const [emailState, dispatchEmail] = useReducer(emailReducer, {
+		value: '',
+		isValid: null,
+	});
+
+	const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+		value: '',
+		isValid: null,
+	});
+
+	const { isValid: emailIsValid } = emailState;
+	const { isValid: passwodIsValid } = passwordState;
+
 	useEffect(() => {
-		setFormIsValid(
-			enteredEmail.includes('@') && enteredPassword.trim().length > 6
-		);
-	}, [enteredEmail, enteredPassword]);
+		const identifier = setTimeout(() => {
+			console.log("Checking validity");
+			setFormIsValid(emailState.isValid && passwordState.isValid);
+		}, 500);
+
+		return () => {
+			console.log("CLEANUP");
+			clearTimeout(identifier);
+		};
+	}, [emailIsValid, passwodIsValid]);
 
 	const emailChangeHandler = (event) => {
-		setEnteredEmail(event.target.value);
+		dispatchEmail({
+			type: reducerActions.USER_INPUT,
+			value: event.target.value,
+		});
 	};
 
 	const passwordChangeHandler = (event) => {
-		setEnteredPassword(event.target.value);
+		dispatchPassword({
+			type: reducerActions.USER_INPUT,
+			value: event.target.value,
+		});
 	};
 
 	const validateEmailHandler = () => {
-		setEmailIsValid(enteredEmail.includes('@'));
+		dispatchEmail({ type: reducerActions.INPUT_BLUR });
 	};
 
 	const validatePasswordHandler = () => {
-		setPasswordIsValid(enteredPassword.trim().length > 6);
+		dispatchPassword({ type: reducerActions.INPUT_BLUR });
 	};
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(enteredEmail, enteredPassword);
+		props.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
@@ -43,26 +87,26 @@ const Login = (props) => {
 			<form onSubmit={submitHandler}>
 				<div
 					className={`${classes.control} ${
-						emailIsValid === false ? classes.invalid : ''
+						emailState.isValid === false ? classes.invalid : ''
 					}`}>
 					<label htmlFor="email">E-Mail</label>
 					<input
 						type="email"
 						id="email"
-						value={enteredEmail}
+						value={emailState.value}
 						onChange={emailChangeHandler}
 						onBlur={validateEmailHandler}
 					/>
 				</div>
 				<div
 					className={`${classes.control} ${
-						passwordIsValid === false ? classes.invalid : ''
+						passwordState.isValid === false ? classes.invalid : ''
 					}`}>
 					<label htmlFor="password">Password</label>
 					<input
 						type="password"
 						id="password"
-						value={enteredPassword}
+						value={passwordState.value}
 						onChange={passwordChangeHandler}
 						onBlur={validatePasswordHandler}
 					/>
---
+++ src/types/Enum.js
@@ -0,0 +1,4 @@
+export const reducerActions = {
+	USER_INPUT: 'USER_INPUT',
+	INPUT_BLUR: 'INPUT_BLUR',
+};
